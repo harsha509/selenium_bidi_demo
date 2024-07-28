ad5d0d790584903700453bfbb8a55d687a5e416a
Sri Harsha <sri_harsha509@hotmail.com>
2024-07-29 00:54:30 +0530

    update netowrk test

--- spec/browserContext.spec.js
+++ spec/browserContext.spec.js
@@ -14,7 +14,7 @@ describe('BrowserContext: Create and listen to event', ()=> {
       .build();
   })
 
-  it('should listen browserContext events', async () => {
+  it('should listen browserContext contextCreated event', async () => {
     const caps = await driver.getCapabilities();
     let WebSocketUrl = caps['map_'].get('webSocketUrl')
     const conn = new BiDi(WebSocketUrl.replace('localhost', '127.0.0.1'));
@@ -25,6 +25,22 @@ describe('BrowserContext: Create and listen to event', ()=> {
 
     // create a tab
     await browsingContext.create({type: 'tab'})
+
+    // print the subscription result
+    console.log(await browsingContext.events.eventSubscriptionData)
+  })
+
+  it('should listen browserContext load event', async () => {
+    const caps = await driver.getCapabilities();
+    let WebSocketUrl = caps['map_'].get('webSocketUrl')
+    const conn = new BiDi(WebSocketUrl.replace('localhost', '127.0.0.1'));
+
+    // Subscribe to events
+    const browsingContext = new BrowsingContext(conn);
+    await browsingContext.events.load();
+
+    await driver.get('https://www.selenium.dev');
+    await driver.sleep(1000);
 
     // print the subscription result
     console.log(await browsingContext.events.eventSubscriptionData)
--- spec/network.spec.js
+++ spec/network.spec.js
@@ -3,8 +3,9 @@ const {Builder, By} = require('selenium-webdriver');
 const Chrome= require('selenium-webdriver/chrome');
 const opts = new Chrome.Options();
 const { BiDi, Network } = require('wd-bidi');
+const net = require("node:net");
 
-describe('Bidi: Input tests', ()=> {
+describe('Bidi: Network tests', ()=> {
     let driver;
 
     before(async ()=> {
@@ -19,12 +20,18 @@ describe('Bidi: Input tests', ()=> {
         let WebSocketUrl = caps['map_'].get('webSocketUrl')
         const conn = new BiDi(WebSocketUrl.replace('localhost', '127.0.0.1'));
         const network = new Network(conn);
-        const params= {
-            phases: ['beforeRequestSent'],
-        }
-        await network.addIntercept(params);
-        await driver.get('https://www.selenium.dev/selenium/web/bidi/logEntryAdded.html');
 
+        await network.events.fetchError();
+        await driver.get('https://the-internet.herokuapp.com/status_codes/500');
+        await driver.sleep(2000);
+        // print the subscription result
+        let response = await network.events.eventSubscriptionData
+        // Convert Map to object
+        const obj = Object.fromEntries(response);
+
+        // Convert object to JSON string
+        const jsonString = JSON.stringify(obj, null, 2);
+        console.log(jsonString);
     })
 
     after(async ()=> await driver.quit())
